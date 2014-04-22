c7ed66bbf0db925b4d0ef62a47de992e2df6f52d
José Luender de Lima Santos <jose.luender@dce.ufpb.br>
2014-04-22 19:23:54 -0300

    Update Professor.java

--- projetopack/Professor.java
+++ projetopack/Professor.java
@@ -3,23 +3,13 @@ package br.ufpb.dce.poo.projetopack;
 
 public class Professor extends Usuario{
 	
-	private String departamento;
-
 	public Professor(String nome, String matricula, String cpf, String departamento){
 		super(nome, matricula, cpf);
-		this.departamento = departamento;
+		super.setDepartamento(departamento);
 	}
 
 	public int getQuantDiasEmprestimo() {
 		return Configuracao.getInstance().getDiasEmprestimoProfessor();
 	}
 	
-	public String getDepartamento(){
-		return this.departamento;
-	}
-	
-	public void setDepartamento(String departamento){
-		this.departamento = departamento;
-	}
-
 }
